601ae88a83aa20a37b252915ce26b5904ba7690b
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2023-01-11 16:53:20 +0100

    Implement the event handlers to use the math logic from calculate.js and operate.js

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,11 +1,86 @@
 import React from 'react';
-import Buttons from './buttons';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = { total: 0, next: null, operation: null };
+  }
+
+  clickReceived = (event) => {
+    const buttonName = event.target.innerText;
+    const answer = calculate(this.state, buttonName);
+    this.setState(answer);
+  };
+
   render() {
+    const { total, operation, next } = this.state;
     return (
-      <div className="cal-operations">
-        <Buttons />
+      <div className="calculator">
+        <div className="answerDisplay">
+          {total}
+          {operation}
+          {next}
+        </div>
+        <div className="btnContainer">
+          <button type="button" onClick={this.clickReceived}>
+            AC
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            +/-
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            %
+          </button>
+          <button type="button" onClick={this.clickReceived} className="mathOperation">
+            &#247;
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            7
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            8
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            9
+          </button>
+          <button type="button" onClick={this.clickReceived} className="mathOperation">
+            x
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            4
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            5
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            6
+          </button>
+          <button type="button" onClick={this.clickReceived} className="mathOperation">
+            -
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            1
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            2
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            3
+          </button>
+          <button type="button" onClick={this.clickReceived} className="mathOperation">
+            +
+          </button>
+          <button type="button" onClick={this.clickReceived} className="zero">
+            0
+          </button>
+          <button type="button" onClick={this.clickReceived}>
+            .
+          </button>
+          <button type="button" onClick={this.clickReceived} className="mathOperation">
+            =
+          </button>
+        </div>
       </div>
     );
   }
